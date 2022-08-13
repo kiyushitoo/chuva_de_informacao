95e02d8c47268450e93614d2389d090f21481ee4
kiyushitoo <evandrootaki3@hotmail.com>
2022-08-13 15:19:59 -0300

    alteracao das 15:19

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 //inicio do projeto
+//e ai
+const express = require('express');
+const port = 3000
+const app = express();
+const routes = express.Router();
+routes.all('/', async (req, res) => {
+    return res.json("alooha");
+});
+app.use(routes);
+
+async function run() {
+    console.log('comecou');
+    app.listen(port);
+}
+run().catch(console.error);
